4c9932fcf729c04ff15611c5ba3bf91aebf9f0f2
Doan379a <doan379a@gmail.com>
2024-04-05 18:28:28 +0700

    sửa lại api thống kê, api đẩy ảnh dịch vụ

--- routes/dichvu_router.js
+++ routes/dichvu_router.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
+const Upload = require("./upload");
 const DVCtl = require("../controllers/dichvu_controller");
-router.post("/adddichvu", DVCtl.adddichvu);
+router.post("/adddichvu", Upload.array("anh", 5), DVCtl.adddichvu);
 router.get("/getListdichvu", DVCtl.getListdichvu);
-router.put("/updatedichvu/:id", DVCtl.updatedichvu);
+router.put("/updatedichvu/:id", Upload.array("anh", 5), DVCtl.updatedichvu);
 router.delete("/deletedichvu/:id", DVCtl.deletedichvu);
 router.get("/getdichvuById/:id", DVCtl.getdichvu);
-
+router.get("/getTop10DichVu", DVCtl.getTop10DichVu);
 
 module.exports = router;
--- routes/thongke-router.js
+++ routes/thongke-router.js
@@ -1,6 +1,18 @@
 var express = require("express");
 var router = express.Router();
 const TKCTL = require("../controllers/thongke_controller");
-router.get("/getthongke", TKCTL.getthongketongtien);
-router.get("/getthongketheongay", TKCTL.getThongKeTongTienTheoNgay);
+router.get("/getthongketongtien", TKCTL.getthongketongtien);
+router.get("/getTongTienVaHoadonTrongNam", TKCTL.getTongTienVaHoadonTrongNam);
+router.get(
+  "/getSoLuongDonHangTrangThaiTrue",
+  TKCTL.getSoLuongDonHangTrangThaiTrue
+);
+
+// router.get(
+//   "/getThongKeTongTienTheoNgayVaThangTraHang",
+//   TKCTL.getThongKeTongTienTheoNgayVaThangTraHang
+// );
+// router.get("/doanhthu_thongso", TKCTL.getthongketdoanhthu);
+// router.get("/doanhthu_in_date", TKCTL.doanhthu_in_date);
+// router.get("/doanhthu_in_month", TKCTL.doanhthu_in_month);
 module.exports = router;
